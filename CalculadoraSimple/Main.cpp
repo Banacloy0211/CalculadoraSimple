998c33064722135fe41fc2cade8f17fb30490405
Jose Banacloy <banacloyjcarcelen@gmail.com>
2021-11-18 20:47:11 +0100

    Calculadora Simple

--- CalculadoraSimple/Main.cpp
+++ CalculadoraSimple/Main.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-	std::cout << "Hola Jose!" << std::endl;
-	std::cout << "Escriba el valor de A:" << std::endl;
-	float A = 0;
-	std::cin >> A;
-	std::cout << "Escriba el valor de B:" << std::endl;
-	float B = 0;
-	std::cin >> B;
-	std::cout << "Que operacion quieres hacer?:" << std::endl;
-	char o = ' ';
-	std::cin >> o;
-	float R = 0;
-	std::cout << "El resultado es: "<< R << std::endl;
+	
+	int A = 0;
+	int B = 0;
+	cout << "Escriba el valor de A:" << endl;
+	cin >> A;
+	cout << "Escriba el valor de B:" << endl;
+	cin >> B;
+	cout << "Primer valor: " << A << "Segundo valor:" << B << endl;
+	int resultado;
+	char operacion;
+	cout << "Que operacion desea hacer? (* o /)" << endl;
+	cin >> operacion;
+	if (operacion == '*') {
+		resultado = A * B;
+	}
+	else {
+		resultado = A / B;
+	}
+	cout << "El Resultado es: " << resultado << endl;
+	int C = 0;
+	int D = 0;
+	cout << "Escribe el valor de C:" << endl;
+	cin >> C;
+	cout << "Escribe el valor de D:" << endl;
+	cin >> D;
+	cout << "Tercer valor: " << C << "Cuarto valor:" << D << endl;
+	int resultado2;
+	char operacion2; 
+	cout << "Que operacion quieres hacer (+,-,* o /)" << endl;
+	cin >> operacion2;
+	if (operacion2 == '+') {
+		resultado2 = C + D;
+	}
+	else if (operacion2 == '-') {
+		resultado2 = C - D;
+	}
+	else if (operacion2 == '*') {
+		resultado2 = C * D;
+	}
+	else {
+		resultado2 = C / D;
+	}
+	cout << "El resultado de la segunda operacion es:" << resultado2 << endl;
+
+
+
 }
