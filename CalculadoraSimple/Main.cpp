a943fb542b408bdbb69342fe381d8e796d3c310b
Jose Banacloy <banacloyjcarcelen@gmail.com>
2021-11-18 12:43:42 +0100

    InputOutput

--- CalculadoraSimple/Main.cpp
+++ CalculadoraSimple/Main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 int main() {
 	std::cout << "Hola Jose!" << std::endl;
+	std::cout << "Escriba el valor de A:" << std::endl;
 	float A = 0;
+	std::cin >> A;
+	std::cout << "Escriba el valor de B:" << std::endl;
 	float B = 0;
-	float R = 0;
+	std::cin >> B;
+	std::cout << "Que operacion quieres hacer?:" << std::endl;
 	char o = ' ';
+	std::cin >> o;
+	float R = 0;
+	std::cout << "El resultado es: "<< R << std::endl;
 }
