87905b688525f1cc74a80c732de3a124e4d6b79b
Jose Banacloy <banacloyjcarcelen@gmail.com>
2021-11-23 11:14:40 +0100

    Imprimir Fotogramas

--- imprimirFotogramas/ImprimirFotogramas/ImprimirFotogramas.cpp
+++ imprimirFotogramas/ImprimirFotogramas/ImprimirFotogramas.cpp
@@ -5,21 +5,37 @@ using namespace std;
 int main()
 {
 	char ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
-	
+	char personaje = 'O';
+	int personaje_x = 10;
+	int personaje_y = 5;
 	for (int i = 0; i < CONSOLE_HEIGHT; i++)
 	{
 		for (int j = 0; j < CONSOLE_WIDTH; j++)
 		{
-			ConsoleScreen[i][j] = '-';
-		}
-		
+			if (i== 0 || i== CONSOLE_HEIGHT - 1 || j == 0 || j == CONSOLE_WIDTH - 1)
+			{
+				ConsoleScreen[i][j] = '#';
+			}
+			else {
+				ConsoleScreen[i][j] = '-';
+			}	
+		}	
+
 	}
 	for (int i = 0; i < CONSOLE_HEIGHT; i++)
 	{
 		for (int j = 0; j < CONSOLE_WIDTH; j++)
 		{
-			cout << ConsoleScreen[i];
+			if (personaje_x == j && personaje_y == i ) {
+				cout << personaje;
+			
+			}
+			else
+			{
+				cout << ConsoleScreen[i][j];
+			}
+			
 		}
-		cout << endl;
+		cout << endl; 
 	}
 }
